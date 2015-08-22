39b04989690978ea34684a119d3a3bdd8663ca7c
Sunny <me@darkowlzz.space>
2015-08-22 16:05:50 +0530

    sidebar and sort added.

--- src/model/ClanController.js
+++ src/model/ClanController.js
@@ -1,5 +1,34 @@
 class ClanController {
   constructor($scope, $mdSidenav, database) {
+
+    Array.prototype.moveUp = function(value, by) {
+        var index = this.indexOf(value),
+            newPos = index - (by || 1);
+
+        if(index === -1)
+            throw new Error("Element not found in array");
+
+        if(newPos < 0)
+            newPos = 0;
+
+        this.splice(index,1);
+        this.splice(newPos,0,value);
+    };
+
+    Array.prototype.moveDown = function(value, by) {
+        var index = this.indexOf(value),
+            newPos = index + (by || 1);
+
+        if(index === -1)
+            throw new Error("Element not found in array");
+
+        if(newPos >= this.length)
+            newPos = this.length;
+
+        this.splice(index, 1);
+        this.splice(newPos,0,value);
+    };
+
     this.scope = $scope;
     this.mdSidenav = $mdSidenav;
     this.database = database;
@@ -16,6 +45,8 @@ class ClanController {
     this.scope.toggleSidenav = function (menuId) {
       $mdSidenav(menuId).toggle();
     };
+
+    
   }
 
   getColor (war) {
@@ -40,6 +71,21 @@ class ClanController {
                                   console.log('update successful');
                                   // change loading status
                                 });
+  }
+
+  moveUp (item) {
+    this.members.moveUp(item);
+    console.log(this.members);
+  }
+
+  moveDown (item) {
+    this.members.moveDown(item);
+    console.log(this.members);
+  }
+
+  toggleSidenav (menuId) {
+    console.log('toggle toggle');
+    this.mdSidenav(menuId).toggle();
   }
 }
 
